6ef07a8efd88a40b90387f43b48949677c8c49e0
Qasim Ameen <qasimbutt332@gmail.com>
2023-10-09 17:06:10 +0500

    Bug fix

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
-
 app.get('/api/:date?', (req, res) => {
   let dateParam = req.params.date;
 
-  // If dateParam is empty, use the current time
   if (!dateParam) {
     dateParam = new Date();
   } else {
-    // Try to parse the dateParam
     const date = new Date(dateParam);
 
-    // Check if the parsed date is valid
     if (!isNaN(date.getTime())) {
       res.json({
         unix: date.getTime(),
@@ -21,13 +16,11 @@ app.get('/api/:date?', (req, res) => {
       });
       return;
     } else {
-      // If the date is invalid, return an error
       res.json({ error: 'Invalid Date' });
       return;
     }
   }
 
-  // If everything else fails, return the current time
   res.json({
     unix: Date.now(),
     utc: new Date().toUTCString(),
