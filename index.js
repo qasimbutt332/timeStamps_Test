63a78d6114006d535aa1b81f69ab561ccf1eff30
Qasim Ameen <qasimbutt332@gmail.com>
2023-10-09 17:00:42 +0500

    Second Commit

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/api/timestamp/:unix', (req, res) => {
-  const { unix } = req.params;
-  const date = new Date(parseInt(unix));
-  if (!isNaN(date.getTime())) {
-    res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString(),
-    });
+app.get('/api/:date?', (req, res) => {
+  let dateParam = req.params.date;
+
+  // If dateParam is empty, use the current time
+  if (!dateParam) {
+    dateParam = new Date();
   } else {
-    res.json({ error: 'Invalid date' });
+    // Try to parse the dateParam
+    const date = new Date(dateParam);
+
+    // Check if the parsed date is valid
+    if (!isNaN(date.getTime())) {
+      res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString(),
+      });
+      return;
+    } else {
+      // If the date is invalid, return an error
+      res.json({ error: 'Invalid Date' });
+      return;
+    }
   }
+
+  // If everything else fails, return the current time
+  res.json({
+    unix: Date.now(),
+    utc: new Date().toUTCString(),
+  });
 });
 
 const PORT = process.env.PORT || 3000;
